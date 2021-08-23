144fc9a3404c24067a31fbe0e5fad7e2a0bea779
m1172 <muhshuh2000@gmail.com>
2021-08-23 23:09:16 +0200

    edited

--- food-networks/src/app.jsx
+++ food-networks/src/app.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar.jsx';
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
   }
 }
